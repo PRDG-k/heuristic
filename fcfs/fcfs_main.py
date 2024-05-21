003cfddaed2709f779e071a9fa1db1dea4302b8f
PRDG-K <lasrb6605@naver.com>
2024-05-21 19:48:20 +0900

    quater-completion ~ consider non-required charge ~ consider discharge

--- fcfs/fcfs_main.py
+++ fcfs/fcfs_main.py
@@ -4,23 +4,27 @@ if __name__ == '__main__':
     import argparse
     from fcfs_module import *
 
-    import argparse
-    parser = argparse.ArgumentParser(description='스크립트에 전달되는 파라미터 처리')
-    parser.add_argument('shave', type=int, default=3462, help='Max peak shaving')
-    parser.add_argument('--k', type=float, default=1, help='ratio')
-    parser.add_argument('--s', type=str, help='seed number')
-    args = parser.parse_args()
-
     current_directory = os.getcwd()
     parent_directory = os.path.dirname(current_directory)
-    parent_directory = os.path.dirname(parent_directory)
+    # parent_directory = os.path.dirname(parent_directory)
+    
+    args = {}
+    with open("fcfs/args.txt", 'r') as file:
+        for line in file:
+            parts = line.strip().split(' ')
+
+            key = parts[0]
+            items = parts[1]
+            args[key] = items
+
 
     # 버스 운행 정보 불러오기
-    import_directory = parent_directory + "/Data/l1/out/" + args.s
+    import_directory = parent_directory + "/Data/l1/out/" + args["s"]
     file_name = "/schedule_range.txt"
 
     schedule = {}
-    soc_status = {}
+    solution = {}
+    charge_finished = {}
 
     with open(import_directory + file_name, 'r') as file:
         for line in file:
@@ -30,11 +34,17 @@ if __name__ == '__main__':
             items = parts[1:]
             # schedule[key] = items
             schedule[key] = sorted(items, key=lambda x: int(x))
-
-            if key[0] == '1':
-                soc_status[key] = 200
-            else:
-                soc_status[key] = 300
+            
+            charge_finished[key] = {False}
+    
+    bus_list = list(schedule.keys())
+    solution['bus'] = [n for n in bus_list]
+    solution['period'] = [0 for _ in bus_list]
+    solution['charge'] = [0 for _ in bus_list]
+    solution['discharge'] = [0 for _ in bus_list]
+    solution['consumption'] = [0 for _ in bus_list]
+    solution['SOC'] = [check_bus_type(n) for n in bus_list]
+    solution = pd.DataFrame(solution)
 
     # 충전소 데이터 불러오기
     # import_directory = parent_directory + "/Data/l2/out/"
@@ -46,8 +56,8 @@ if __name__ == '__main__':
     T = range(1, NT+1)
 
     on_station = on_station_list(schedule, NT)
-    soultion = pd.DataFrame(columns=['bus', 'period', 'charge', 'discharge', 'consumption'])
-
+    on_charging = []
+    
     consumption = 0.25
     charge = 0.4166667
 
@@ -57,21 +67,35 @@ if __name__ == '__main__':
     start_time = time.time()  # 시작 시간
 
     for t in T:
-        for bus, on in on_station.items():
-            # 운행중
-            # print(f"{on[0]} ~~ {t}")
+        print(f"TNOW: {t}")
+        
+        for bus in on_station:
+            L = on_station[bus]
+
+            if charge_finished[bus] == True:
+                continue
+
             try:
-                if on[0] != str(t):
-                    soc_status[bus] = soc_status[bus] - consumption
+                filt= (solution['bus'] == bus) & (solution['period'] == t-1)
+                _soc = solution.loc[filt, 'SOC'].values
+
+                if L[0] != t:
+                    _sol = bus_departure(on_charging, bus, t, _soc)
                 else:
-                    required = calculate_demand(bus, soc_status[bus])
-                    if required > 0:
-                        soc_status[bus] = soc_status[bus] + charge
-            
-                del on[0]
+                    _sol = bus_charging(on_charging, bus, t, _soc)
+                    del L[0]
+                
+                solution = pd.concat([solution, pd.DataFrame(_sol)], ignore_index=True)
+
             except IndexError:
-                print(f"{bus}: finished")
+                print(f"\nPERIOD: {t}")
+                print(f"---BUS: {bus} finished---")
+                charge_finished[key] = True
+                
     
     end_time = time.time()  # 종료 시간
     elapsed_time = end_time - start_time  # 작동 시간 계산
     print(f"\n코드 작동 시간: {elapsed_time:.4f}초")
+
+    solution = solution.sort_values(by=['bus', 'period'])
+    solution.to_csv("fcfs_solution.csv")
--- fcfs/fcfs_module.py
+++ fcfs/fcfs_module.py
@@ -1,3 +1,11 @@
+def check_bus_type(n):
+    capa = {'day_bus' : 250,
+            'n_bus' : 350}
+    if n[0] == "1":
+        return capa['day_bus']
+    else:
+        return capa['n_bus']
+
 def on_station_list(schedules, nt):
     T = set(range(1,nt + 1))
     
@@ -18,4 +26,30 @@ def calculate_demand(bus, soc):
         return capa['day_bus'] * 0.7 - soc
     else:
         return capa['n_bus'] * 0.7 - soc
+    
+def remove_bus(charging_list, bus):
+    if bus in charging_list:
+        charging_list.remove(bus)
 
+def bus_departure(charging_list, bus, t, soc):
+
+    remove_bus(charging_list, bus)
+
+    consumption = 0.25
+    sol = {'bus': bus, 'period': t, "charge": 0, "discharge": 0, "consumption": consumption, "SOC": soc - consumption}
+
+    return sol
+
+def bus_charging(charging_list, bus, t, soc):
+
+    charge = 0.4166667
+    required = calculate_demand(bus, soc)
+
+    if (required > 0) & (len(charging_list) <= 30):
+        _sol = {'bus': bus, 'period': t, "charge": charge, "discharge": 0, "consumption": 0, "SOC": soc + charge}
+        charging_list.append(bus)
+    else:
+        _sol = {'bus': bus, 'period': t, "charge": 0, "discharge": 0, "consumption": 0, "SOC": soc}
+        remove_bus(charging_list, bus)
+
+    return _sol
